832a9dd0c362d43c9a9a5bb257f0f6bd96fdbaee
Misha2007 <mishadrogovoz@gmail.com>
2025-03-17 20:22:34 +0200

    Setting up the project

---
+++ src/main/java/com/internship/air_tickets_system/AirTicketsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.internship.air_tickets_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTicketsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirTicketsSystemApplication.class, args);
+	}
+
+}
